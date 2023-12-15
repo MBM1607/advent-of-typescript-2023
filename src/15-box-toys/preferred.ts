63d41db010bb9f58b9c7ef2a13bc3f3f421b2fed
mbm_1607 <muhammadkhan1607@gmail.com>
2023-12-15 16:58:42 +0500

    :art: Use union distribution for advantage

--- src/15-box-toys/preferred.ts
+++ src/15-box-toys/preferred.ts
@@ -2,6 +2,6 @@ export type MakeTuple<
 	T,
 	Length extends number,
 	Tuple extends T[] = [],
-> = Tuple["length"] extends Length ? Tuple : MakeTuple<T, Length, [...Tuple, T]>;
+> = Length extends Tuple["length"] ? Tuple : MakeTuple<T, Length, [...Tuple, T]>;
 
-export type BoxToys<T, N extends number> = N extends N ? MakeTuple<T, N> : never;
+export type BoxToys<T, N extends number> = MakeTuple<T, N>;
